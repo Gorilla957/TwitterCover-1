41641733ab80004fb4c2a2648ea49143f56581cb
duduluk101 <87215279@qq.com>
2014-06-26 11:42:31 +0800

    ..

--- TwitterCoverDemo/TwitterCoverDemo/ViewController.m
+++ TwitterCoverDemo/TwitterCoverDemo/ViewController.m
@@ -29,11 +29,24 @@
     // Do any additional setup after loading the view from its nib.
     
     UIImageView *topImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 180)];
+    topImage.contentMode = UIViewContentModeScaleAspectFill;
     topImage.layer.masksToBounds = YES;
+    
+    //网络图片
     NSString *str = @"http://img1.gtimg.com/9/975/97536/9753640_980x1200_292.jpg";
+    [self.scrollView addTwitterCoverWithImage:str];
+    
+    //本地图片
+//    topImage.image = [UIImage imageNamed:@"image.jpg"];
+//    [self.scrollView addTwitterCoverWithImage:topImage];
+    
+    
+    
+    
+    
     
     [self.scrollView setContentSize:CGSizeMake(320, 700)];
-    [self.scrollView addTwitterCoverWithImage:str];
+
 }
 
 - (void)didReceiveMemoryWarning
